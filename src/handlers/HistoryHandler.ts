a375b4adc143d40687ecc5cfc297b34b26ddacb1
brendanjones44 <brendanpj44@gmail.com>
2021-12-31 09:48:27 -0800

    history command implementation

--- src/handlers/HistoryHandler.ts
+++ src/handlers/HistoryHandler.ts
@@ -20,73 +20,92 @@ class HistoryHandler implements CommandHandler {
         // Get all deposits
         // Get all stakes
         // From each stake, get all transactions that have a sale
-        // From each transaction 
+        // From each transaction
+        
+        const amounts: { date: Date, eth: number, description: string }[] = [];
 
-        // const transactionCollection = collection(db, 'opensea-transactions');
-        // const transactionQuery = query(transactionCollection, where('assetLink', '==', interaction.options.getString('opensea_link')))
-        // const transactionQueryResult = await getDocs(transactionQuery);
-        // const numberMatchingRecords = transactionQueryResult.size;
-
-        // if (numberMatchingRecords === 0) {
-        //     await interaction.reply(`No matching transactions found for: ${interaction.options.getString('opensea_link')}\nYou either have a typo or this has not been recorded yet`);
-        //     return;
-        // }
-
-        // if (numberMatchingRecords === 1) {
-        //     const transactionRecord = transactionQueryResult.docs[0];
-
-        //     const purchasePrice = transactionRecord.get('purchasePrice');
-        //     const purchaseFee = transactionRecord.get('purchaseFees');
-        //     const totalCost = purchasePrice + purchaseFee;
-
-        //     // TODO: Need to handle conditional logic if the asset hasn't sold yet
-        //     const salePrice = transactionRecord.get('salePrice');
-        //     const saleFee = transactionRecord.get('saleFees');
-        //     const netSale = salePrice - saleFee;
-
-        //     const equityCollection = collection(db, 'equity-stakes');
-        //     const equityQuery = query(equityCollection, where('assetLink', '==', interaction.options.getString('opensea_link')));
-        //     const equityQueryResult = await getDocs(equityQuery);
-
-        //     let equityReplyString = '    Equity Breakdown\n';
-
-        //     if (equityQueryResult.size === 0) {
-        //         equityReplyString += '        No equity owners found';
-        //     } else {
-        //         equityQueryResult.forEach(equityStake => {
-        //             const amountEthPaid = equityStake.get('amountEthPaid');
-        //             const portionOfAsset = amountEthPaid / totalCost;
-        //             const amountEthOwed = portionOfAsset * netSale;
-
-        //             equityReplyString += `        Breakdown for ${equityStake.get('discordDisplayName')}:\n`;
-        //             equityReplyString += `            Paid ${amountEthPaid} ETH on ${equityStake.get('stakeDate').toDate()}\n`;
-        //             equityReplyString += `            Results in ${amountEthPaid} / ${totalCost} ownership which is: ${portionOfAsset * 100} %\n`;
-        //             equityReplyString += `            ETH owed after sale: ${amountEthOwed}\n`;
-        //             equityReplyString += `            Profited ${amountEthOwed - amountEthPaid} ETH (${(amountEthOwed / amountEthPaid) * 100} % return)\n`;
-        //         });
-        //     }
+        // Get all deposits
+        const depositCollection = collection(db, 'deposits');
+        const depositQuery = query(depositCollection, where('discordDisplayName', '==', discordName));
+        const depositQueryResult = await getDocs(depositQuery);
+        if (depositQueryResult.size === 0) {
+            await interaction.reply(`No history found for ${discordName}`);
+            return;
+        }
+        depositQueryResult.docs.forEach(deposit => {
+            amounts.push({
+                date: deposit.get('depositDate').toDate(),
+                eth: deposit.get('ethAmount'),
+                description: 'Deposit into the fund'
+            });
+        })
 
 
-        //     await interaction.reply(
-        //         `Describing: ${interaction.options.getString('opensea_link')}\n` +
-        //         '    Purchase Breakdown\n' +
-        //         `        Purchased for: ${purchasePrice} ETH on ${transactionRecord.get('purchaseDate').toDate()}\n` +
-        //         `        Purchase link: ${transactionRecord.get('etherScanPurchaseLink')}\n` +
-        //         `        Purchase fees: ${purchaseFee} ETH (${transactionRecord.get('purchaseFeeDescription')})\n` +
-        //         '    Sale Breakdown\n' +
-        //         `        Sold for: ${salePrice} ETH on ${transactionRecord.get('saleDate').toDate()}\n` +
-        //         `        Sale link: ${transactionRecord.get('etherScanSaleLink')}\n` +
-        //         `        Sale fees: ${saleFee} ETH (${transactionRecord.get('saleFeeDescription')})\n` +
-        //         '    Return Breakdown\n' +
-        //         `        Net Sale: ${netSale} ETH\n` +
-        //         `        Total Cost: ${totalCost} ETH\n` +
-        //         `        Profit: ${netSale - totalCost} ETH\n` +
-        //         `        % Return: ${(netSale / totalCost) * 100}\n` + equityReplyString
-        //     );
-        //     return;
-        // }
+        // Get all stakes
+        const equityStakeCollection = collection(db, 'equity-stakes');
+        const equityStakeQuery = query(equityStakeCollection, where('discordDisplayName', '==', discordName));
+        const equityStakeQueryResult = await getDocs(equityStakeQuery);
+        if (equityStakeQueryResult.size === 0) {
+            await interaction.reply(`No history found for ${discordName}`);
+            return;
+        }
 
-        await interaction.reply(`History for ${discordName}`)
+        // From each stake, find the corresponding asset transaction if it has been sold
+        for await (const equityStake of equityStakeQueryResult.docs) {
+            // Keep track of amounts deposited
+            amounts.push({
+                date: equityStake.get('stakeDate').toDate(),
+                eth: equityStake.get('amountEthPaid') * -1, // withdrawing from fund to stake
+                description: `Equity stake in ${equityStake.get('assetLink')}`
+            });
+
+            const assetLink = equityStake.get('assetLink');
+
+            const transactionCollection = collection(db, 'opensea-transactions');
+            const transactionQuery = query(transactionCollection, where('assetLink', '==', assetLink));
+            const transactionQueryResult = await getDocs(transactionQuery);
+
+            if (transactionQueryResult.size === 1) {
+                const transactionMatchingEquityStake = transactionQueryResult.docs[0];
+
+                const purchasePrice = transactionMatchingEquityStake.get('purchasePrice');
+                const purchaseFee = transactionMatchingEquityStake.get('purchaseFees');
+                const totalCost = purchasePrice + purchaseFee;
+
+                // TODO: Need to handle conditional logic if the asset hasn't sold yet
+                const salePrice = transactionMatchingEquityStake.get('salePrice');
+                const saleFee = transactionMatchingEquityStake.get('saleFees');
+                const netSale = salePrice - saleFee;
+
+                const amountEthPaid = equityStake.get('amountEthPaid');
+                const portionOfAsset = amountEthPaid / totalCost;
+                const amountEthOwed = portionOfAsset * netSale;
+
+                amounts.push({
+                    date: transactionMatchingEquityStake.get('saleDate').toDate(),
+                    eth: amountEthOwed,
+                    description: `Payout for owning ${(portionOfAsset * 100).toPrecision(5)}% of ${assetLink}`
+                });
+            } else {
+                console.log(`No transaction found for assetLink: ${assetLink}`);
+            }
+        }
+
+        let replyString = '';
+        let balance = 0;
+        amounts.sort((amountA, amountB) => amountA.date.getTime() - amountB.date.getTime()).forEach(amount => {
+            balance += amount.eth;
+            replyString += `${amount.date.toLocaleString()}: ${amount.eth.toPrecision(5)} ETH for ${amount.description}\n`
+            // Don't display very small values
+            if (balance < 0.001 && balance > -0.001) {
+                balance = 0;
+            }
+            replyString += `Balance: ${balance.toPrecision(5)} ETH\n===\n`
+        });
+
+        console.log(replyString);
+
+        await interaction.reply("History logged for Ethan")
         // Assuming the same WoW is not bought and sold multiple times
         // await interaction.reply('Multiple transactions found for this asset! Not supported yet!')
     }
